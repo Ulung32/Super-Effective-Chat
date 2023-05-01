163d6e833faddba8e5f3b9132ab3719a23abb260
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-05-01 13:53:38 +0700

    refactor: change get day function to public

--- src/Backend/feature/date.go
+++ src/Backend/feature/date.go
@@ -7,7 +7,7 @@ import (
 	// "log"
 )
 
-func getDay(dateString string) (string, error) {
+func GetDay(dateString string) (string, error) {
 	dateFormats := []string{"02/01/2006", "01-02-2006", "2006-01-02", "2 January 2006", "2006/01/02", "02/1/2006", "2/01/2006", "2 Jan 2006", "1-02-2006", "01-2-2006", "1-2-2006", "1/2/2006"}
 	var date time.Time
 	var err error
