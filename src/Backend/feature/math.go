a0d456bf3ba1c0d06cfcca5d2dfca6b46af8494a
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-05-01 13:54:06 +0700

    refactor: add division by 0 exception

--- src/Backend/feature/math.go
+++ src/Backend/feature/math.go
@@ -1,32 +1,35 @@
 package feature
 
 import (
+    "errors"
+    "github.com/Knetic/govaluate"
+    "math"
 	// "fmt"
-	"github.com/Knetic/govaluate"
-	"errors"
-	// "log"
-	// "math"
-	// "strings"
-	// "strconv"
 )
 
 func MathematicalOperationSolver(mathematicalExpression string) (float64, error) {
-	expression, err := govaluate.NewEvaluableExpression(mathematicalExpression)
-	if(err != nil){
-		return 0, errors.New("Invalid Expression")
-	}
-	result, err := expression.Evaluate(nil)
-	if(err != nil){
-		return 0, errors.New("Invalid Expression")
-	}
-	return result.(float64), nil
+    expression, err := govaluate.NewEvaluableExpression(mathematicalExpression)
+    if err != nil {
+        return 0, errors.New("Invalid Expression")
+    }
+    result, err := expression.Evaluate(nil)
+    if err != nil {
+        return 0, errors.New("Invalid Expression")
+    }
+    if val, ok := result.(float64); ok {
+        if math.IsInf(val, 0) {
+            return 0, errors.New("Division by zero")
+        }
+    }
+    return result.(float64), nil
 }
 
-// func main(){
-// 	expression := "1+2+((3*5)"
+// func main() {
+// 	expression := "1+2+(3*5)/0"
 // 	result, status := MathematicalOperationSolver(expression)
-// 	if(status != nil){
-// 		log.Fatal(status)
+// 	if status != nil {
+// 		fmt.Println(status)
+// 	} else {
+// 		fmt.Println("This is the result", result)
 // 	}
-// 	fmt.Println("This is the result", result)
 // }
